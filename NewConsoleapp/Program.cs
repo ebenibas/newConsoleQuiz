e5d5bdc1f2c2bd45fd9e239ed7f10aa9c83ad58f
Ebenezer Obaseki <ebenibas@outlook.com>
2019-08-15 15:49:40 -0400

    Add project files.

---
+++ NewConsoleapp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewConsoleapp
+{
+    public class Program
+    {
+        public static int LeastFrequent(int[] a)
+        {
+            int value = 0;
+            for (int i = 0; i < a.Length; i++)
+                value = value ^ a[i];
+            return value;
+
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 4, 2, 1, 3,3, 4, 2,7};
+            int once = LeastFrequent(a);
+
+            Console.WriteLine("Value that appears once is {0}.", once);
+            Console.WriteLine(HasDuplicates(a));
+            Console.ReadLine();
+        }
+
+        public static bool HasDuplicates(int[] a)
+        {
+         
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int x = i + 1; x < a.Length; x++)
+                {
+                    if (a[i] == a[x]) return true;
+                }
+            }
+            return false;
+            
+        }
+       
+
+    }
+}
---
+++ quizTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("quizTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("quizTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("950c7fe4-5770-42a7-989b-ff57609d4d97")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
